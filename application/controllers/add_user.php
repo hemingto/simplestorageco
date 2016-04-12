6e3f7e4728889c79dd55afaf61abf2ba40b42f36
Calvin Hemington <calvin.hemington@gmail.com>
2016-04-12 16:38:29 -0700

    updates model

--- application/controllers/add_user.php
+++ application/controllers/add_user.php
@@ -5,7 +5,7 @@ class Add_User extends CI_Controller {
 
 	public function index()
 	{
-		// $this->load->helper('url');
+		$this->load->helper('url');
 		$this->load->view('add_user_form');
 	}
 	public function process() 
--- application/models/users.php
+++ application/models/users.php
@@ -6,6 +6,8 @@ class Users extends CI_Model
 		$query = "INSERT INTO users (first_name, last_name, email, password, phone_number, street, city, state, zip, country, created_at, updated_at)
 				  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())";
 		$values = array($user['first_name'], $user['last_name'], $user['email'], $user['password'], $user['phone_number'], $user['street'], $user['city'], $user['state'], $user['zip'], $user['country']);
+		$user_id = $this->db->insert_id();
+		$this->session->set_userdata($user_id);
 		return $this->db->query($query, $values);
 	}
 }
